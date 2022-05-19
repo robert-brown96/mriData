c635264a0635628255b7443b00f447d3e0e69523
robert-brown96 <robert.brown@saascg.com>
2022-05-19 18:46:18 -0500

    customer frame working

--- __tests__/customer.test.js
+++ __tests__/customer.test.js
@@ -11,41 +11,41 @@ const testTable = [
     { name: "Fee Manager", internal_id: 4 }
 ];
 describe("Test Converting MultiSelect to Number Array", () => {
-    test("Successfully Converts to Numbfer Array Length 1", () => {
+    test("Successfully Converts to Numbfer Array Length 1", async () => {
         const testVal = "Owner/GP";
-        const testConvert = convert_multi_select(testTable, testVal);
+        const testConvert = await convert_multi_select(testTable, testVal);
         expect(testConvert).toEqual([7]);
     });
 
-    test("Successfully Converts to Numbfer Array Length 4", () => {
+    test("Successfully Converts to Numbfer Array Length 4", async () => {
         const testVal = "Developer;Owner/GP;Owner/Operator;Fee Manager";
-        const testConvert = convert_multi_select(testTable, testVal);
+        const testConvert = await convert_multi_select(testTable, testVal);
         expect(testConvert).toEqual([1, 7, 3, 4]);
     });
 
-    test("Successfully Converts to Numbfer Array Length 4", () => {
+    test("Successfully Converts to Numbfer Array Length 4", async () => {
         const testVal = "";
-        const testConvert = convert_multi_select(testTable, testVal);
-        expect(testConvert).to;
+        const testConvert = await convert_multi_select(testTable, testVal);
+        expect(testConvert).toEqual([]);
     });
 });
 
 describe("Test Trimming Names", () => {
-    test("Successfully returns concatenated client id and company name", () => {
+    test("Successfully returns concatenated client id and company name", async () => {
         const client_id = "V621999";
         const name = "Test Customer";
-        const testConvert = trimNames(client_id, name);
+        const testConvert = await trimNames(client_id, name);
         const expected = { companyname: name, name: `${client_id} ${name}` };
         expect(testConvert).toMatchObject(expected);
     });
 
-    test("Successfully returns concatenated client id and company name for over 80", () => {
+    test("Successfully returns concatenated client id and company name for over 80", async () => {
         const client_id = "ZQ70999";
         const name =
             "BRADFIELD AND PRICHARD REAL ESTATE CONSULTANTS PTY LTD ATF BENSON AND GUTH UNIT TRUST";
         const concatname =
             "BRADFIELD AND PRICHARD REAL ESTATE CONSULTANTS PTY LTD ATF BENSON AND GUTH UNIT";
-        const testConvert = trimNames(client_id, name);
+        const testConvert = await trimNames(client_id, name);
         const expected = {
             companyname: concatname,
             name: `${client_id} ${name}`.substring(0, 79)
--- object-scripts/customer.js
+++ object-scripts/customer.js
@@ -49,9 +49,9 @@ const sfAccountQl = `SELECT
                     FROM
                         sf_accounts a
                     WHERE
-                        a. "Id" NOT IN(
+                        a."Client_Id__c" NOT IN(
                             SELECT
-                                sf_id FROM trans_customers);
+                                client_id FROM trans_customers) LIMIT 25000;
                     `;
 
 /**
@@ -60,24 +60,27 @@ const sfAccountQl = `SELECT
  * @param {*} value
  * @returns
  */
-module.exports.convert_multi_select = (table, value) => {
-    const tableUse = table.reduce(
-        (o, key) => ({
-            ...o,
-            [key.name]: key.internal_id
-        }),
-        {}
-    );
-    let split = value.split(";");
-    let return_value = [];
-    for (let i = 0; i < split.length; i++) {
-        let t = split[i];
-        if (t) {
-            let temp = tableUse[t.trim()];
-            if (temp && temp !== "") return_value.push(temp);
+const convert_multi_select = async (table, value) => {
+    return await new Promise(resolve => {
+        //    console.log(table);
+        const tableUse = table.reduce(
+            (o, key) => ({
+                ...o,
+                [key.name]: key.internal_id
+            }),
+            {}
+        );
+        let split = value.split(";");
+        let return_value = [];
+        for (let i = 0; i < split.length; i++) {
+            let t = split[i];
+            if (t) {
+                let temp = tableUse[t.trim()];
+                if (temp && temp !== "") return_value.push(temp);
+            }
         }
-    }
-    return return_value;
+        resolve(return_value);
+    });
 };
 
 /**
@@ -86,10 +89,106 @@ module.exports.convert_multi_select = (table, value) => {
  * @param {String} name
  * @returns {{companyname,name}}
  */
-module.exports.trimNames = (client_id, name) => {
-    const concat = `${client_id} ${name}`;
+const trimNames = async (client_id, name) => {
+    try {
+        const concat = `${client_id} ${name}`;
+        return {
+            companyname: name.substring(0, 79),
+            name: concat.substring(0, 79)
+        };
+    } catch (e) {
+        console.log(`ERROR FIR ${client_id} and ${name}`);
+        console.error(e);
+    }
+};
+
+/**
+ *
+ * @param {SfAccount} sfAccount
+ * @returns {TransCustomer}
+ */
+const transformAccount = async (sfAccount, mappingTables) => {
+    //console.log("ta" + JSON.stringify(sfAccount));
+    const nameObj = await trimNames(sfAccount.client_id, sfAccount.companyname);
+
+    const residential = convert_multi_select(
+        mappingTables.residentialClass,
+        sfAccount.residential_prop_class
+    );
+    const commercial = convert_multi_select(
+        mappingTables.commercialClass,
+        sfAccount.commercial_prop_class
+    );
+    const corporate = convert_multi_select(
+        mappingTables.corporateClass,
+        sfAccount.corporate_prop_class
+    );
+
+    const client_type = await convert_multi_select(
+        mappingTables.clientTypes,
+        sfAccount.client_type
+    );
+    const [rps, comps, corps] = await Promise.all([
+        residential,
+        commercial,
+        corporate
+    ]);
     return {
-        companyname: name.substring(0, 79),
-        name: concat.substring(0, 79)
+        client_id: sfAccount.client_id,
+        companyname: nameObj.companyname,
+        name: nameObj.name,
+        sf_id: sfAccount.sf_id,
+        client_type: client_type.join(","),
+        customer_class: [...rps, ...comps, ...corps].join(",")
     };
+};
+
+const streamer = async () => {
+    try {
+        const dbServ = new DbService();
+        const clientTypesProm = dbServ.lookupTable("map_clienttype");
+        const commercialClassProm = dbServ.lookupTable("map_clienttype");
+        const corporateClassProm = dbServ.lookupTable("map_clienttype");
+        const residentialClassProm = dbServ.lookupTable("map_clienttype");
+        const [clientTypes, commercialClass, corporateClass, residentialClass] =
+            await Promise.all([
+                clientTypesProm,
+                commercialClassProm,
+                corporateClassProm,
+                residentialClassProm
+            ]);
+
+        await dbServ.etl({
+            query: sfAccountQl,
+            transformFunc: transformAccount,
+            tableName: "trans_customers",
+            conflict: "client_id",
+            mappingTables: {
+                clientTypes: clientTypes,
+                commercialClass: commercialClass,
+                corporateClass: corporateClass,
+                residentialClass: residentialClass
+            }
+        });
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+const writeOut = async () => {
+    const dbServ = new DbService();
+    const d = require("../lib/db/data.json");
+    dbServ.bulk_insert({
+        bulkData: d,
+        tableName: "trans_customers",
+        conflict: "client_id"
+    });
+};
+
+module.exports = {
+    convert_multi_select,
+    trimNames,
+    transformAccount,
+    streamer,
+    writeOut
 };
