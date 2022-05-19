9ed0d9a880f67b19352b2ac5a3ded61865190916
robert-brown96 <robert.brown@saascg.com>
2022-05-19 09:31:10 -0500

    add customer types

--- object-scripts/customer.js
+++ object-scripts/customer.js
@@ -1,7 +1,4 @@
-
-
-
-
+const DbService = require("../lib/db/db-service");
 
 // get data from db
 //  read into stream
@@ -12,5 +9,29 @@
  * companyname
  * clienttype
  * customerclass
- * 
-*/
+ *
+ */
+/**
+ * @typedef {Object} SfAccount
+ * @property {String} sf_id
+ * @property {String} client_id
+ * @property {String} client_type
+ * @property {String} companyname
+ * @property {String} residential_prop_class
+ * @property {String} commercial_prop_class
+ * @property {String} corporate_prop_class
+ *
+ */
+/**
+ * @typedef {Object} TransCustomer
+ * @property {String} sf_id
+ * @property {String} client_id
+ * @property {Number[]} client_type
+ * @property {String} companyname
+ * @property {String} name
+ * @property {Number[]} residential_prop_class
+ * @property {Number[]} commercial_prop_class
+ * @property {Number[]} corporate_prop_class
+ *
+ */
+
